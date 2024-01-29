99a9a3e40b22a89a5929fd74f916784321d69b2a
Thomas.py <143884468+Tomikeu@users.noreply.github.com>
2024-01-29 07:57:52 +0000

    news

--- news.py
+++ news.py
@@ -1,3 +1,24 @@
-x = 5E10
-
-print(x)
+#x = 5E10
+#print(x)
+#
+#y = """Ahoj
+#Ahoj"""
+#
+#for x in "ahoj":
+#    print(x)
+#
+#list = ["ahoj", "ne", "ano"]
+#for x in list:
+#  print(x)
+#
+#l = "AhOj"
+#u = "aHoJ"
+#print = (l.upper())
+#print = (u.lower())
+#
+#
+#k = "ahoj"
+#print(k.replace("a", "n"))
+#
+#r = "AHOJ"
+#print(r)
