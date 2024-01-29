534beb4e9e1a519d0c3ab4924c30bf768dd5417a
Thomas.py <143884468+Tomikeu@users.noreply.github.com>
2024-01-29 08:28:24 +0000

    uvitani

---
+++ uvitani.py
@@ -0,0 +1,5 @@
+n = input("Jak se jmenuješ ?")
+i = "Tomas Tomáš Matěj Matej Lukas Lukáš Matyas Matyáš Daniel Tobias Tobiáš Samuel Tadeas Tadeáš Jonas Jonáš"
+o = "Honza Vojta"
+e = "Jakub Jan Adam"
+
